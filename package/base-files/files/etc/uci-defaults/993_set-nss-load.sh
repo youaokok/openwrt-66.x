bd0f888c4d93ffe19dab7a2f4aa745dd9951ff08
youaokok <zzhl10010@gmail.com>
2024-12-22 05:47:41 +0800

    Update 993_set-nss-load.sh

--- package/base-files/files/etc/uci-defaults/993_set-nss-load.sh
+++ package/base-files/files/etc/uci-defaults/993_set-nss-load.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #指定文件路径
-FILE="/usr/share/rpcd/ucode/luci"
+LUCI_FILE="/usr/share/rpcd/ucode/luci"
 
-#添加NSS状态显示
-sed -i "s#const fd = popen('top -n1 | awk \\\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\\'')#const fd = popen(access('/sbin/cpuusage') ? '/sbin/cpuusage' : \"top -n1 | awk \\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\'\")#g" $FILE
+# 对luci文件做修改，添加NSS Load相关显示
+sed -i "s#const fd = popen('top -n1 | awk \\\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\\'')#const fd = popen(access('/sbin/cpuusage') ? '/sbin/cpuusage' : \"top -n1 | awk \\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\'\")#g" $LUCI_FILE
 
 exit 0
