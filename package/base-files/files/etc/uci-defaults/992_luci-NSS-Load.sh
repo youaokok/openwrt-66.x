f7c5c548d9e92b2525ba127e3ef644e730a5100d
youaokok <zzhl10010@gmail.com>
2024-12-26 18:44:12 +0800

    Delete package/base-files/files/etc/uci-defaults/992_luci-NSS-Load.sh

--- package/base-files/files/etc/uci-defaults/992_luci-NSS-Load.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 指定luci文件所在目录
-luci_dir="/usr/share/rpcd/ucode/luci"
-
-# 对luci文件做修改，添加NSS Load相关显示
-sed -i "s#const fd = popen('top -n1 | awk \\\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\\'')#const fd = popen(access('/sbin/cpuusage') ? '/sbin/cpuusage' : \"top -n1 | awk \\'/^CPU/ {printf(\"%d%\", 100 - \$8)}\\'\")#g" $LUCI_FILE
-
-exit 0
