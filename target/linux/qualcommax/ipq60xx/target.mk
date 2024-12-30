388d3832b5b571f7c198f35229c61e41e58f14c7
breeze303 <grapenskrskr@gmail.com>
2024-12-30 21:35:51 +0800

    qualcommax: fix default options

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -1,9 +1,7 @@
 SUBTARGET:=ipq60xx
 BOARDNAME:=Qualcomm Atheros IPQ60xx
 DEFAULT_PACKAGES += \
-	ath11k-firmware-ipq6018 nss-firmware-ipq6018 kmod-qca-nss-dp kmod-ath11k-ahb \
-	kmod-qca-nss-drv kmod-qca-nss-ecm \
-	kmod-qca-nss-drv-bridge-mgr
+	ath11k-firmware-ipq6018 nss-firmware-ipq6018 kmod-qca-nss-dp kmod-ath11k-ahb
 
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ60xx based boards.
