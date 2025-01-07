66361be585cad65bd4f64e15e57218eb1280dfa5
breeze303 <grapenskrskr@gmail.com>
2025-01-07 15:22:23 +0800

    package: nss_diag update

--- package/kernel/mac80211/files/nss_diag.sh
+++ package/kernel/mac80211/files/nss_diag.sh
@@ -17,6 +17,9 @@ fi
 # Retrieve OpenWRT version
 [ -r /etc/openwrt_version ] && openwrt_rev=$(cat /etc/openwrt_version)
 
+# Retrieve Linux kernel
+kernel=$(uname -r)
+
 # Retrieve device model
 model=$(jsonfilter -e ''@.model.name'' < /etc/board.json | sed -e "s/,/_/g")
 
@@ -39,6 +42,7 @@ ipq_date=${IPQ_DATE:-"N/A"}
 
 # Defaults for empty variables
 openwrt_rev=${openwrt_rev:-"N/A"}
+kernel=${kernel:-"N/A"}
 model=${model:-"N/A"}
 nss_version=${nss_version:-"N/A"}
 ath11k_fw=${ath11k_fw:-"N/A"}
@@ -47,6 +51,7 @@ mac80211_version=${mac80211_version:-"N/A"}
 # Display the information
 echo -e "${bold}${red}     MODEL${reset}: ${blue}${bold}${model}${reset}"
 echo -e "${bold}${red}   OPENWRT${reset}: ${white}${openwrt_rev}${reset}"
+echo -e "${bold}${red}    KERNEL${reset}: ${yellow}${kernel}${reset}"
 echo -e "${bold}${red}IPQ BRANCH${reset}: ${cyan}${ipq_branch}${reset}"
 echo -e "${bold}${red}IPQ COMMIT${reset}: ${cyan}${ipq_commit}${reset}"
 echo -e "${bold}${red}  IPQ DATE${reset}: ${cyan}${ipq_date}${reset}"
@@ -74,11 +79,28 @@ done
 echo -e "${reset}"
 echo -ne "${bold}${red}  NSS PKGS${reset}: ${white}"
 
-apk list -I | awk -v count=0 '
+if cmd=$(command -v apk) > /dev/null; then
+  flags="list -I"
+else
+  cmd=$(command -v opkg)
+  flags="list-installed"
+fi
+
+if [ -z "$cmd" ]; then
+  echo -e "${red}No package manager found${reset}"
+  exit 1
+fi
+
+$cmd $flags | awk -v count=0 -v cmd=$cmd '
   /kmod-qca|^nss/ {
   if(count>0) tab="            "
-  print tab $1
+  print tab (cmd == "/bin/opkg" ? $0 : $1)
   count++
-}'
+  }
+  END {
+    if (count == 0) {
+      print "N/a"
+    }
+  }'
 
 echo -ne "${reset}"
